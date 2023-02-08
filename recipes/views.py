fd2dc736367d1a485a5d6d2953d02f3c1683a3d9
Guilherme Mendonça <guilhermemef@hotmail.com>
2023-02-08 16:31:16 -0400

    criando inicial

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Guilherme'})
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
+    return render(request, 'recipes/pages/home.html')
